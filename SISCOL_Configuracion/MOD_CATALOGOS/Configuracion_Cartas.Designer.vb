b87c8f4fcb2e6b0e814d2b85fc76d1ec8301280c
ArkhamBoy14 <samus1454@gmail.com>
2023-02-01 13:31:47 -0600

    Se quitan todos los campos

--- SISCOL_Configuracion/MOD_CATALOGOS/Configuracion_Cartas.Designer.vb
+++ SISCOL_Configuracion/MOD_CATALOGOS/Configuracion_Cartas.Designer.vb
@@ -21,140 +21,35 @@ Partial Class Configuracion_Cartas
     'It can be modified using the Wisej Designer.  
     'Do not modify it using the code editor.
     Private Sub InitializeComponent()
-        Me.opcDRO = New Wisej.Web.RadioButton()
-        Me.AspNetPanel1 = New SISCOL_Configuracion.AspNetPanelII()
-        Me.GroupBox4 = New Wisej.Web.GroupBox()
-        Me.opcRTEC = New Wisej.Web.RadioButton()
-        Me.rb_FechaActual = New Wisej.Web.RadioButton()
-        Me.Cbx_DRO = New SISCOL_Configuracion.ComboWisax()
-        Me.Cbx_RTEC = New SISCOL_Configuracion.ComboWisax()
-        Me.Cbx_Secretario = New SISCOL_Configuracion.ComboWisax()
-        Me.Cbx_Presidente = New SISCOL_Configuracion.ComboWisax()
-        Me.Label4 = New Wisej.Web.Label()
-        Me.rb_EsCopia = New Wisej.Web.RadioButton()
+        Me.Panel1 = New Wisej.Web.Panel()
         Me.GroupBox2 = New Wisej.Web.GroupBox()
+        Me.Cmb_CategoriaCSE = New Wisej.Web.ComboBox()
+        Me.Chk_AplicaCSE = New Wisej.Web.CheckBox()
         Me.Cmb_Clasificacion = New Wisej.Web.ComboBox()
         Me.Chk_Clasificacion = New Wisej.Web.CheckBox()
         Me.Chk_Especifica = New Wisej.Web.CheckBox()
         Me.Label5 = New Wisej.Web.Label()
         Me.Dtp_FechaCarta = New Wisej.Web.DateTimePicker()
-        Me.Label3 = New Wisej.Web.Label()
-        Me.Label2 = New Wisej.Web.Label()
-        Me.Btn_Guardar = New Wisej.Web.Button()
-        Me.GroupBox3 = New Wisej.Web.GroupBox()
-        Me.Label1 = New Wisej.Web.Label()
-        Me.GroupBox1 = New Wisej.Web.GroupBox()
-        Me.SplitContainer1 = New Wisej.Web.SplitContainer()
-        Me.Cmb_CategoriaCSE = New Wisej.Web.ComboBox()
-        Me.Chk_AplicaCSE = New Wisej.Web.CheckBox()
-        Me.GroupBox4.SuspendLayout()
+        Me.rb_EsCopia = New Wisej.Web.RadioButton()
+        Me.rb_FechaActual = New Wisej.Web.RadioButton()
+        Me.CheckBox1 = New Wisej.Web.CheckBox()
+        Me.Panel1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
-        Me.GroupBox3.SuspendLayout()
-        Me.GroupBox1.SuspendLayout()
-        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SplitContainer1.Panel1.SuspendLayout()
-        Me.SplitContainer1.Panel2.SuspendLayout()
-        Me.SplitContainer1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'opcDRO
+        'Panel1
         '
-        Me.opcDRO.Checked = True
-        Me.opcDRO.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel)
-        Me.opcDRO.Location = New System.Drawing.Point(108, 26)
-        Me.opcDRO.Name = "opcDRO"
-        Me.opcDRO.Size = New System.Drawing.Size(62, 22)
-        Me.opcDRO.TabIndex = 0
-        Me.opcDRO.TabStop = True
-        Me.opcDRO.Text = "DRO"
-        '
-        'AspNetPanel1
-        '
-        Me.AspNetPanel1.Dock = Wisej.Web.DockStyle.Fill
-        Me.AspNetPanel1.Location = New System.Drawing.Point(0, 66)
-        Me.AspNetPanel1.Name = "AspNetPanel1"
-        Me.AspNetPanel1.Size = New System.Drawing.Size(453, 412)
-        Me.AspNetPanel1.TabIndex = 4
-        Me.AspNetPanel1.Text = "AspNetPanelII1"
-        '
-        'GroupBox4
-        '
-        Me.GroupBox4.Controls.Add(Me.opcRTEC)
-        Me.GroupBox4.Controls.Add(Me.opcDRO)
-        Me.GroupBox4.Dock = Wisej.Web.DockStyle.Top
-        Me.GroupBox4.Location = New System.Drawing.Point(0, 0)
-        Me.GroupBox4.Name = "GroupBox4"
-        Me.GroupBox4.Size = New System.Drawing.Size(453, 66)
-        Me.GroupBox4.TabIndex = 3
-        Me.GroupBox4.Text = "Vista Previa"
-        '
-        'opcRTEC
-        '
-        Me.opcRTEC.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel)
-        Me.opcRTEC.Location = New System.Drawing.Point(313, 26)
-        Me.opcRTEC.Name = "opcRTEC"
-        Me.opcRTEC.Size = New System.Drawing.Size(69, 22)
-        Me.opcRTEC.TabIndex = 1
-        Me.opcRTEC.Text = "RTEC"
-        '
-        'rb_FechaActual
-        '
-        Me.rb_FechaActual.Checked = True
-        Me.rb_FechaActual.Location = New System.Drawing.Point(8, 21)
-        Me.rb_FechaActual.Name = "rb_FechaActual"
-        Me.rb_FechaActual.Size = New System.Drawing.Size(108, 22)
-        Me.rb_FechaActual.TabIndex = 3
-        Me.rb_FechaActual.TabStop = True
-        Me.rb_FechaActual.Text = "Fecha Actual"
-        '
-        'Cbx_DRO
-        '
-        Me.Cbx_DRO.Location = New System.Drawing.Point(17, 196)
-        Me.Cbx_DRO.Name = "Cbx_DRO"
-        Me.Cbx_DRO.Size = New System.Drawing.Size(363, 22)
-        Me.Cbx_DRO.TabIndex = 7
-        '
-        'Cbx_RTEC
-        '
-        Me.Cbx_RTEC.Location = New System.Drawing.Point(17, 147)
-        Me.Cbx_RTEC.Name = "Cbx_RTEC"
-        Me.Cbx_RTEC.Size = New System.Drawing.Size(363, 22)
-        Me.Cbx_RTEC.TabIndex = 6
-        '
-        'Cbx_Secretario
-        '
-        Me.Cbx_Secretario.Location = New System.Drawing.Point(17, 100)
-        Me.Cbx_Secretario.Name = "Cbx_Secretario"
-        Me.Cbx_Secretario.Size = New System.Drawing.Size(363, 22)
-        Me.Cbx_Secretario.TabIndex = 5
-        '
-        'Cbx_Presidente
-        '
-        Me.Cbx_Presidente.Location = New System.Drawing.Point(17, 46)
-        Me.Cbx_Presidente.Name = "Cbx_Presidente"
-        Me.Cbx_Presidente.Size = New System.Drawing.Size(363, 22)
-        Me.Cbx_Presidente.TabIndex = 4
-        '
-        'Label4
-        '
-        Me.Label4.Anchor = Wisej.Web.AnchorStyles.Left
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(18, 198)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(91, 15)
-        Me.Label4.TabIndex = 3
-        Me.Label4.Text = "Comisión DRO"
-        '
-        'rb_EsCopia
-        '
-        Me.rb_EsCopia.Location = New System.Drawing.Point(122, 21)
-        Me.rb_EsCopia.Name = "rb_EsCopia"
-        Me.rb_EsCopia.Size = New System.Drawing.Size(118, 22)
-        Me.rb_EsCopia.TabIndex = 4
-        Me.rb_EsCopia.Text = "Fecha Anterior"
+        Me.Panel1.Controls.Add(Me.GroupBox2)
+        Me.Panel1.Dock = Wisej.Web.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(436, 469)
+        Me.Panel1.TabIndex = 0
+        Me.Panel1.TabStop = True
         '
         'GroupBox2
         '
+        Me.GroupBox2.Controls.Add(Me.CheckBox1)
         Me.GroupBox2.Controls.Add(Me.Cmb_CategoriaCSE)
         Me.GroupBox2.Controls.Add(Me.Chk_AplicaCSE)
         Me.GroupBox2.Controls.Add(Me.Cmb_Clasificacion)
@@ -165,11 +60,27 @@ Partial Class Configuracion_Cartas
         Me.GroupBox2.Controls.Add(Me.rb_EsCopia)
         Me.GroupBox2.Controls.Add(Me.rb_FechaActual)
         Me.GroupBox2.Dock = Wisej.Web.DockStyle.Top
-        Me.GroupBox2.Location = New System.Drawing.Point(3, 18)
+        Me.GroupBox2.Location = New System.Drawing.Point(0, 0)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(487, 168)
-        Me.GroupBox2.TabIndex = 3
+        Me.GroupBox2.Size = New System.Drawing.Size(436, 265)
+        Me.GroupBox2.TabIndex = 4
         Me.GroupBox2.Text = "Fecha de Impresión"
+        '
+        'Cmb_CategoriaCSE
+        '
+        Me.Cmb_CategoriaCSE.Items.AddRange(New Object() {"Categoría A", "Categoría B", "Categoría C"})
+        Me.Cmb_CategoriaCSE.Location = New System.Drawing.Point(174, 125)
+        Me.Cmb_CategoriaCSE.Name = "Cmb_CategoriaCSE"
+        Me.Cmb_CategoriaCSE.Size = New System.Drawing.Size(222, 22)
+        Me.Cmb_CategoriaCSE.TabIndex = 12
+        '
+        'Chk_AplicaCSE
+        '
+        Me.Chk_AplicaCSE.Location = New System.Drawing.Point(8, 125)
+        Me.Chk_AplicaCSE.Name = "Chk_AplicaCSE"
+        Me.Chk_AplicaCSE.Size = New System.Drawing.Size(168, 22)
+        Me.Chk_AplicaCSE.TabIndex = 11
+        Me.Chk_AplicaCSE.Text = "Aplica Categoría (CSE)"
         '
         'Cmb_Clasificacion
         '
@@ -214,153 +125,57 @@ Partial Class Configuracion_Cartas
         Me.Dtp_FechaCarta.TabIndex = 6
         Me.Dtp_FechaCarta.Value = New Date(2022, 8, 4, 10, 51, 54, 212)
         '
-        'Label3
+        'rb_EsCopia
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(17, 128)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(98, 15)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Comisión RTEC"
+        Me.rb_EsCopia.Location = New System.Drawing.Point(122, 21)
+        Me.rb_EsCopia.Name = "rb_EsCopia"
+        Me.rb_EsCopia.Size = New System.Drawing.Size(118, 22)
+        Me.rb_EsCopia.TabIndex = 4
+        Me.rb_EsCopia.Text = "Fecha Anterior"
         '
-        'Label2
+        'rb_FechaActual
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(17, 79)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(64, 15)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Secretario"
+        Me.rb_FechaActual.Checked = True
+        Me.rb_FechaActual.Location = New System.Drawing.Point(8, 21)
+        Me.rb_FechaActual.Name = "rb_FechaActual"
+        Me.rb_FechaActual.Size = New System.Drawing.Size(108, 22)
+        Me.rb_FechaActual.TabIndex = 3
+        Me.rb_FechaActual.TabStop = True
+        Me.rb_FechaActual.Text = "Fecha Actual"
         '
-        'Btn_Guardar
+        'CheckBox1
         '
-        Me.Btn_Guardar.Location = New System.Drawing.Point(280, 228)
-        Me.Btn_Guardar.Name = "Btn_Guardar"
-        Me.Btn_Guardar.Size = New System.Drawing.Size(100, 60)
-        Me.Btn_Guardar.TabIndex = 8
-        Me.Btn_Guardar.Text = "Guardar"
-        '
-        'GroupBox3
-        '
-        Me.GroupBox3.Controls.Add(Me.Btn_Guardar)
-        Me.GroupBox3.Controls.Add(Me.Cbx_DRO)
-        Me.GroupBox3.Controls.Add(Me.Cbx_RTEC)
-        Me.GroupBox3.Controls.Add(Me.Cbx_Secretario)
-        Me.GroupBox3.Controls.Add(Me.Cbx_Presidente)
-        Me.GroupBox3.Controls.Add(Me.Label4)
-        Me.GroupBox3.Controls.Add(Me.Label3)
-        Me.GroupBox3.Controls.Add(Me.Label2)
-        Me.GroupBox3.Controls.Add(Me.Label1)
-        Me.GroupBox3.Dock = Wisej.Web.DockStyle.Top
-        Me.GroupBox3.Location = New System.Drawing.Point(3, 186)
-        Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(487, 299)
-        Me.GroupBox3.TabIndex = 4
-        Me.GroupBox3.Text = "Firmas de Cartas"
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(17, 25)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(67, 15)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Presidente"
-        '
-        'GroupBox1
-        '
-        Me.GroupBox1.Controls.Add(Me.GroupBox3)
-        Me.GroupBox1.Controls.Add(Me.GroupBox2)
-        Me.GroupBox1.Dock = Wisej.Web.DockStyle.Fill
-        Me.GroupBox1.Location = New System.Drawing.Point(0, 0)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(493, 478)
-        Me.GroupBox1.TabIndex = 1
-        Me.GroupBox1.Text = "Configuración de Cartas"
-        '
-        'SplitContainer1
-        '
-        Me.SplitContainer1.Dock = Wisej.Web.DockStyle.Fill
-        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
-        Me.SplitContainer1.Name = "SplitContainer1"
-        '
-        'SplitContainer1.Panel1
-        '
-        Me.SplitContainer1.Panel1.Controls.Add(Me.GroupBox1)
-        Me.SplitContainer1.Panel1.TabStop = True
-        '
-        'SplitContainer1.Panel2
-        '
-        Me.SplitContainer1.Panel2.Controls.Add(Me.AspNetPanel1)
-        Me.SplitContainer1.Panel2.Controls.Add(Me.GroupBox4)
-        Me.SplitContainer1.Panel2.TabStop = True
-        Me.SplitContainer1.Size = New System.Drawing.Size(956, 480)
-        Me.SplitContainer1.SplitterDistance = 495
-        Me.SplitContainer1.TabIndex = 4
-        '
-        'Cmb_CategoriaCSE
-        '
-        Me.Cmb_CategoriaCSE.Items.AddRange(New Object() {"Categoría A", "Categoría B", "Categoría C"})
-        Me.Cmb_CategoriaCSE.Location = New System.Drawing.Point(174, 125)
-        Me.Cmb_CategoriaCSE.Name = "Cmb_CategoriaCSE"
-        Me.Cmb_CategoriaCSE.Size = New System.Drawing.Size(222, 22)
-        Me.Cmb_CategoriaCSE.TabIndex = 12
-        '
-        'Chk_AplicaCSE
-        '
-        Me.Chk_AplicaCSE.Location = New System.Drawing.Point(8, 125)
-        Me.Chk_AplicaCSE.Name = "Chk_AplicaCSE"
-        Me.Chk_AplicaCSE.Size = New System.Drawing.Size(168, 22)
-        Me.Chk_AplicaCSE.TabIndex = 11
-        Me.Chk_AplicaCSE.Text = "Aplica Categoría (CSE)"
+        Me.CheckBox1.Location = New System.Drawing.Point(8, 153)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(168, 22)
+        Me.CheckBox1.TabIndex = 13
+        Me.CheckBox1.Text = "Aplica Categoría (CSE)"
         '
         'Configuracion_Cartas
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
         Me.AutoScaleMode = Wisej.Web.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(956, 480)
-        Me.Controls.Add(Me.SplitContainer1)
+        Me.ClientSize = New System.Drawing.Size(436, 469)
+        Me.Controls.Add(Me.Panel1)
         Me.Name = "Configuracion_Cartas"
         Me.Text = "Configuracion_Cartas"
-        Me.GroupBox4.ResumeLayout(False)
-        Me.GroupBox4.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
         Me.GroupBox2.ResumeLayout(False)
         Me.GroupBox2.PerformLayout()
-        Me.GroupBox3.ResumeLayout(False)
-        Me.GroupBox3.PerformLayout()
-        Me.GroupBox1.ResumeLayout(False)
-        Me.SplitContainer1.Panel1.ResumeLayout(False)
-        Me.SplitContainer1.Panel2.ResumeLayout(False)
-        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.SplitContainer1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents opcDRO As RadioButton
-    Friend WithEvents AspNetPanel1 As AspNetPanelII
-    Friend WithEvents GroupBox4 As GroupBox
-    Friend WithEvents opcRTEC As RadioButton
-    Friend WithEvents rb_FechaActual As RadioButton
-    Friend WithEvents Cbx_DRO As ComboWisax
-    Friend WithEvents Cbx_RTEC As ComboWisax
-    Friend WithEvents Cbx_Secretario As ComboWisax
-    Friend WithEvents Cbx_Presidente As ComboWisax
-    Friend WithEvents Label4 As Label
-    Friend WithEvents rb_EsCopia As RadioButton
+    Friend WithEvents Panel1 As Panel
     Friend WithEvents GroupBox2 As GroupBox
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Btn_Guardar As Button
-    Friend WithEvents GroupBox3 As GroupBox
-    Friend WithEvents Label1 As Label
-    Friend WithEvents GroupBox1 As GroupBox
-    Friend WithEvents SplitContainer1 As SplitContainer
-    Friend WithEvents Dtp_FechaCarta As DateTimePicker
-    Friend WithEvents Label5 As Label
-    Friend WithEvents Chk_Especifica As CheckBox
-    Friend WithEvents Cmb_Clasificacion As ComboBox
-    Friend WithEvents Chk_Clasificacion As CheckBox
     Friend WithEvents Cmb_CategoriaCSE As ComboBox
     Friend WithEvents Chk_AplicaCSE As CheckBox
+    Friend WithEvents Cmb_Clasificacion As ComboBox
+    Friend WithEvents Chk_Clasificacion As CheckBox
+    Friend WithEvents Chk_Especifica As CheckBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Dtp_FechaCarta As DateTimePicker
+    Friend WithEvents rb_EsCopia As RadioButton
+    Friend WithEvents rb_FechaActual As RadioButton
+    Friend WithEvents CheckBox1 As CheckBox
 End Class
--- SISCOL_Configuracion/MOD_CATALOGOS/Configuracion_Cartas.vb
+++ SISCOL_Configuracion/MOD_CATALOGOS/Configuracion_Cartas.vb
@@ -8,15 +8,15 @@ Public Class Configuracion_Cartas
     Dim Alertas As New Notificaciones
     Private Sub Configuracion_Inicial_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         CargarCombos()
-        ImprimirDocumento()
+        'ImprimirDocumento()
     End Sub
     Sub CargarCombos()
         ReDim Utilidades.ParametersX_Global(0)
         Utilidades.ParametersX_Global(0) = New SqlParameter("@ACTUAL", "SI")
-        Cbx_Presidente.LlenarListBox("pSOCIOS_CONSEJO_CARGOS", "Cve_Socio", "NombreCompletoX", Utilidades.ParametersX_Global)
-        Cbx_Secretario.LlenarListBox("pSOCIOS_CONSEJO_CARGOS", "Cve_Socio", "NombreCompletoX", Utilidades.ParametersX_Global)
-        Cbx_DRO.LlenarListBox("pSOCIOS_CONSEJO_CARGOS", "Cve_Socio", "NombreCompletoX", Utilidades.ParametersX_Global)
-        Cbx_RTEC.LlenarListBox("pSOCIOS_CONSEJO_CARGOS", "Cve_Socio", "NombreCompletoX", Utilidades.ParametersX_Global)
+        'Cbx_Presidente.LlenarListBox("pSOCIOS_CONSEJO_CARGOS", "Cve_Socio", "NombreCompletoX", Utilidades.ParametersX_Global)
+        'Cbx_Secretario.LlenarListBox("pSOCIOS_CONSEJO_CARGOS", "Cve_Socio", "NombreCompletoX", Utilidades.ParametersX_Global)
+        'Cbx_DRO.LlenarListBox("pSOCIOS_CONSEJO_CARGOS", "Cve_Socio", "NombreCompletoX", Utilidades.ParametersX_Global)
+        'Cbx_RTEC.LlenarListBox("pSOCIOS_CONSEJO_CARGOS", "Cve_Socio", "NombreCompletoX", Utilidades.ParametersX_Global)
         CargarDatos()
     End Sub
 
@@ -39,10 +39,10 @@ Public Class Configuracion_Cartas
 
                     End If
 
-                    Cbx_Presidente.SelectedValue = DataReader.Item("Firma_Presidente")
-                    Cbx_Secretario.SelectedValue = DataReader.Item("Firma_Secretario")
-                    Cbx_DRO.SelectedValue = DataReader.Item("Firma_Comision_DRO")
-                    Cbx_RTEC.SelectedValue = DataReader.Item("Firma_Comision_RTEC")
+                    'Cbx_Presidente.SelectedValue = DataReader.Item("Firma_Presidente")
+                    'Cbx_Secretario.SelectedValue = DataReader.Item("Firma_Secretario")
+                    'Cbx_DRO.SelectedValue = DataReader.Item("Firma_Comision_DRO")
+                    'Cbx_RTEC.SelectedValue = DataReader.Item("Firma_Comision_RTEC")
                     Chk_Clasificacion.Checked = DataReader.Item("AplicaCategoriaDRO")
                     Cmb_Clasificacion.SelectedItem = DataReader.Item("CategoriaDRO")
                     Chk_AplicaCSE.Checked = DataReader.Item("AplicaCategoriaCSE")
@@ -68,10 +68,10 @@ Public Class Configuracion_Cartas
         Try
             ReDim Utilidades.ParametersX_Global(10)
             Utilidades.ParametersX_Global(0) = New SqlParameter("@EsCopia", rb_EsCopia.Checked)
-            Utilidades.ParametersX_Global(1) = New SqlParameter("@Firma_Presidente", Cbx_Presidente.SelectedValue)
-            Utilidades.ParametersX_Global(2) = New SqlParameter("@Firma_Secretario", Cbx_Secretario.SelectedValue)
-            Utilidades.ParametersX_Global(3) = New SqlParameter("@Firma_Comision_DRO", Cbx_DRO.SelectedValue)
-            Utilidades.ParametersX_Global(4) = New SqlParameter("@Firma_Comision_RTEC", Cbx_RTEC.SelectedValue)
+            'Utilidades.ParametersX_Global(1) = New SqlParameter("@Firma_Presidente", Cbx_Presidente.SelectedValue)
+            'Utilidades.ParametersX_Global(2) = New SqlParameter("@Firma_Secretario", Cbx_Secretario.SelectedValue)
+            'Utilidades.ParametersX_Global(3) = New SqlParameter("@Firma_Comision_DRO", Cbx_DRO.SelectedValue)
+            'Utilidades.ParametersX_Global(4) = New SqlParameter("@Firma_Comision_RTEC", Cbx_RTEC.SelectedValue)
             Utilidades.ParametersX_Global(5) = New SqlParameter("@Fecha_Especifica", Chk_Especifica.Checked)
             Utilidades.ParametersX_Global(6) = New SqlParameter("@Fecha_Impresion", Dtp_FechaCarta.Value)
             Utilidades.ParametersX_Global(7) = New SqlParameter("@AplicaCategoriaDRO", Chk_Clasificacion.Checked)
@@ -91,53 +91,54 @@ Public Class Configuracion_Cartas
         End Try
 
     End Sub
-    Private Sub Btn_Guardar_Click(sender As Object, e As EventArgs) Handles Btn_Guardar.Click
+    Private Sub Btn_Guardar_Click(sender As Object, e As EventArgs)
         Guardar()
-        ImprimirDocumento()
+        'ImprimirDocumento()
     End Sub
 
-    Sub ImprimirDocumento()
-        If opcDRO.Checked Then
-            GenerarReporte("R_CARTA_DRO_CACHAC")
-        End If
-        If opcRTEC.Checked Then
-            GenerarReporte("R_CARTAS_RTEC_CACHAC")
-        End If
-    End Sub
+    'Sub ImprimirDocumento()
+    '    If opcDRO.Checked Then
+    '        GenerarReporte("R_CARTA_DRO_CACHAC")
+    '    End If
+    '    If opcRTEC.Checked Then
+    '        GenerarReporte("R_CARTAS_RTEC_CACHAC")
+    '    End If
+    'End Sub
 
-    Sub GenerarReporte(ByVal sReportName As String)
-        Try
-            If Trim(sReportName <> Nothing) Then
-                AspNetPanel1.PageSource = Nothing
-                Application.Session("ReportName") = sReportName
-                Application.Session("DocumentCached") = Nothing
-                AspNetPanel1.PageSource = "ReportView.aspx"
-                Me.AspNetPanel1.Update()
-            End If
-        Catch ex As Exception
-            MessageBox.Show(ex.Message, "Cargar Reporte", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-        End Try
-    End Sub
+    'Sub GenerarReporte(ByVal sReportName As String)
+    '    Try
+    '        If Trim(sReportName <> Nothing) Then
+    '            AspNetPanel1.PageSource = Nothing
+    '            Application.Session("ReportName") = sReportName
+    '            Application.Session("DocumentCached") = Nothing
+    '            AspNetPanel1.PageSource = "ReportView.aspx"
+    '            Me.AspNetPanel1.Update()
+    '        End If
+    '    Catch ex As Exception
+    '        MessageBox.Show(ex.Message, "Cargar Reporte", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+    '    End Try
+    'End Sub
 
-    Private Sub opcDRO_CheckedChanged(sender As Object, e As EventArgs) Handles opcDRO.CheckedChanged
-        ImprimirDocumento()
-    End Sub
+    'Private Sub opcDRO_CheckedChanged(sender As Object, e As EventArgs)
+    '    ImprimirDocumento()
+    'End Sub
 
 
 
-    Private Sub Chk_Especifica_CheckedChanged(sender As Object, e As EventArgs) Handles Chk_Especifica.CheckedChanged
-        If Chk_Especifica.Checked Then
-            Dtp_FechaCarta.Enabled = True
-        Else
-            Dtp_FechaCarta.Enabled = False
-        End If
-    End Sub
+    'Private Sub Chk_Especifica_CheckedChanged(sender As Object, e As EventArgs)
+    '    If Chk_Especifica.Checked Then
+    '        Dtp_FechaCarta.Enabled = True
+    '    Else
+    '        Dtp_FechaCarta.Enabled = False
+    '    End If
+    'End Sub
 
-    Private Sub Chk_Clasificacion_CheckedChanged(sender As Object, e As EventArgs) Handles Chk_Clasificacion.CheckedChanged
-        If Chk_Clasificacion.Checked = True Then
-            Cmb_Clasificacion.Enabled = True
-        Else
-            Cmb_Clasificacion.Enabled = False
-        End If
-    End Sub
+    'Private Sub Chk_Clasificacion_CheckedChanged(sender As Object, e As EventArgs)
+    '    If Chk_Clasificacion.Checked = True Then
+    '        Cmb_Clasificacion.Enabled = True
+    '    Else
+    '        Cmb_Clasificacion.Enabled = False
+    '    End If
+    'End Sub
+
 End Class
